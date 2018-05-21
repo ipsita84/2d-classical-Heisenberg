ebb52bf1a19ece2e6b4e07e31ef45df8e81693ea
Ipsita <ipsita@dhcp-10-36-16-241.access.cit.cornell.edu>
2018-05-21 11:30:22 -0400

    updates

--- autocorrelation.cc
+++ autocorrelation.cc
@@ -1,4 +1,5 @@
 // g++ -std=c++11 -Wall -O3 autocorrelation.cc -o test
+//Reference: https://ocw.mit.edu/courses/chemistry/5-74-introductory-quantum-mechanics-ii-spring-2004/lecture-notes/11.pdf or https://arxiv.org/pdf/0905.1629.pdf
 
 #include <iostream>
 #include <fstream>
@@ -19,6 +20,8 @@ int main(int argc, char const * argv[])
 
         }
 	gin.close();
+       
+        ofstream fout("autocorr.dat");	// Opens a file for output
 	
 	double en_avg(0);
 	double en_square_avg(0);
@@ -30,17 +33,20 @@ int main(int argc, char const * argv[])
          }
          en_avg = en_avg / N_mc ;
          en_square_avg = en_square_avg / N_mc ;
+         cout << "avg energy" << en_avg <<endl ;
+         cout << "avg energy square" << en_square_avg  << endl;
          
         double autosum(0); 
-        for (unsigned int i=0; i < N_mc; ++i )
+        for (unsigned int i=1; i < N_mc-1; ++i )
         {
         
                double sum(0);
-               for (unsigned int j=1+i;j < N_mc -i; ++j )
+               for (unsigned int j=0; j < N_mc -i; ++j )
                {
-			sum += energy[j]*energy[j+i] - en_square_avg ;
+			sum += (energy[j]- en_avg)*(energy[j+i] - en_avg) ;
 
-                }
+                } 
+                fout<< i << '\t' << sum/ (N_mc - i) <<endl ;
                 autosum +=  sum/(N_mc - i);
 
          }
@@ -48,7 +54,7 @@ int main(int argc, char const * argv[])
         double autocorr_time =  autosum / (en_square_avg - en_avg * en_avg);
         cout << autocorr_time;
 
-       	ofstream fout("0.dat", std::fstream::app);	// Opens a file for output
+       
         fout << autocorr_time << endl;
         fout.close();
 
